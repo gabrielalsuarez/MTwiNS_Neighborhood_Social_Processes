821e72ba2dfd602e78f5c460088d2fdc7d932eb3
Gabriela L Suarez <127572000+gabrielalsuarez@users.noreply.github.com>
2023-03-11 10:25:06 -0500

    Add files via upload

---
+++ Stage2_Analysis/Threat_CollectiveEfficacy_Moderation/readargs.R
@@ -0,0 +1,7 @@
+cmdargs <- c("-m","rAAL_BiAmy_PickAtlas.nii", "--set1", "con_0009.txt",             
+             "--setlabels1", "covariates.csv",
+             "--model", "mplus_model.R",
+             "--output", "output/aim3_",
+             "--debug", "debug_npoint.Rdata",
+             "-p", 6
+             )
