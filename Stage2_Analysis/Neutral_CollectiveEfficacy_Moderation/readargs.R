0df6b36ee5041e1ec8bab141fc4b1a853ac2cf4b
Gabriela L Suarez <127572000+gabrielalsuarez@users.noreply.github.com>
2023-03-11 10:34:57 -0500

    Delete readargs.R

--- Stage2_Analysis/Neutral_CollectiveEfficacy_Moderation/readargs.R
+++
@@ -1,6 +0,0 @@
-cmdargs <- c("-m","rAAL_BiAmy_PickAtlas.nii", "--set1", "con_0015.txt",             
-             "--setlabels1", "covariates.csv",
-             "--model", "mplus_model.R",
-             "--output", "output/aim2_",
-             "--debug", "debug_npoint.Rdata",
-             "-p", 6)
